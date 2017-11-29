889800da00b8721d5e292e88404241d729c05951
kjschelling <kjschelling@gmail.com>
2017-11-29 15:44:40 -0500

    Added relationships between user and mannequins
    -User has many mannequins
    -Mannequins belongs to user

--- app/models/mannequin.rb
+++ app/models/mannequin.rb
@@ -1,2 +1,3 @@
 class Mannequin < ApplicationRecord
+  belongs_to :user
 end
--- app/serializers/mannequin_serializer.rb
+++ app/serializers/mannequin_serializer.rb
@@ -1,3 +1,3 @@
 class MannequinSerializer < ActiveModel::Serializer
-  attributes :id, :given_name, :surname, :height, :weight, :age, :measurements, :eye_color, :hair_color, :hair_length, :gender
+  attributes :id, :given_name, :surname, :height, :weight, :measurements, :eye_color, :hair_color, :hair_length, :gender
 end
