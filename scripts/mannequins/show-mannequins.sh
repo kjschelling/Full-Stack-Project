31fe025ff09c3acb44752865fbf4ae2a9626aa72
kjschelling <kjschelling@gmail.com>
2017-12-01 12:26:35 -0500

    Updated curl scripts

--- scripts/mannequins/show-mannequins.sh
+++ scripts/mannequins/show-mannequins.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 API="${API_ORIGIN:-http://localhost:4741}"
-URL_PATH="/mannequins"/${ID} \
+URL_PATH="/mannequins" \
 curl "${API}${URL_PATH}" \
   --include \
   --request GET \
+  --header "Content-Type: application/json" \
+  --header "Authorization: Token token=$TOKEN" \
 
 echo
